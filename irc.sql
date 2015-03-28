25692cb92f5c1b3f0a376460b69763d5a5887fee
Savannah <stuart.savannah916@gmail.com>
2015-03-28 15:49:45 -0400

    fixed some small things in the irc.sql file

--- irc.sql
+++ irc.sql
@@ -1,10 +1,5 @@
 DROP DATABASE IF EXISTS irc_db;
 CREATE DATABASE irc_db;
-\c irc_db;
-
-create user irc_user with password 'irc';
-grant select, insert on irc_db to irc_user;
-grant all on sequence users_id_seq to irc_user;
 
 CREATE TABLE users (
 	id serial,
