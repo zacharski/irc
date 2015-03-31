995574394eb331ba6b760de31f16636dfd791b93
Savannah <stuart.savannah916@gmail.com>
2015-03-31 01:08:41 -0400

    skj;dfa

--- server.py
+++ server.py
@@ -94,7 +94,7 @@ def getRoomId(roomname):
     except:
         print "could not execute select room id"
         traceback.print_exc()
-    return id
+    return id_dict
 
 #we also need a thing that pulls up messages from a chat
 #maybe have a subscribe function that determines whether or not join is called??
